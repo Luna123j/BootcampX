2685a30cada9bf3c7712519e075542f8009e2bac
Shenglun Jin <sunny.jsl.7@gmail.com>
2022-07-25 17:43:33 +0000

    add queries

---
+++ 1_queries/3_students_without_email_phone.sql
@@ -0,0 +1,9 @@
+SELECT
+  name,
+  id,
+  cohort_id
+FROM
+  students
+WHERE
+  email IS NULL
+  AND phone IS NULL;
---
+++ 1_queries/4_student_without_gmail_phone.sql
@@ -0,0 +1,10 @@
+SELECT
+  name,
+  email,
+  id,
+  cohort_id
+FROM
+  students
+WHERE
+  email not LIKE ('%gmail.com')
+  AND phone IS NULL;
